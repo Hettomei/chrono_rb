e96f1723ab76332b0b3d5f388c488ba51f723c2d
Timothée Gauthier <timothee@teezily.com>
2017-02-22 23:38:33 +0100

    Explain why this patch on ChronoRb in spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,15 @@ require "chrono_rb"
 
 # This way, do not call real abort
 # during test. Because too hard to handle
+#
+# Without this, If my code call exit_with_error
+# every other tests are skipped, and classic
+# rspec trace is printed.
+#
+# That mean, If I had 333 total test to do
+# but it fail on 330th test
+# Nothing will tel me that is something wrong
+#
 module ChronoRb
   def self.exit_with_error(msg)
     warn(msg)
