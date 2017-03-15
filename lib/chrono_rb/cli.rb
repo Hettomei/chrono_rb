c88b20ab5a1098b5eac9304d400c483b0d5e160c
Hettomei <itsumo.sibyllin@gmail.com>
2017-03-15 22:53:59 +0100

    Better CLI desc. Better list group. Better output

--- lib/chrono_rb/cli.rb
+++ lib/chrono_rb/cli.rb
@@ -7,8 +7,9 @@ require 'chrono_rb/stop'
 
 module ChronoRb
   class CLI < Thor
+    GROUP_DESC="[--group=GROUP]"
 
-    desc "start", "Start chrono. Must be stopped"
+    desc "start #{GROUP_DESC}", "Start chrono. Must be stopped."
     option :group, aliases: [:g]
     def start
       conf.set_group(options[:group]) if options[:group]
@@ -18,25 +19,7 @@ module ChronoRb
       display_group
     end
 
-    desc "del", "delete the last entry"
-    option :group, aliases: [:g]
-    def del
-      conf.set_group(options[:group]) if options[:group]
-      del = Delete.new(config: conf)
-      del.del
-      puts "deleting last entry #{del.del} for group #{conf.group}"
-      display_group
-    end
-
-    desc "roots", "all groups"
-    def roots
-      conf.store.roots.each do |entry|
-        next if entry == conf.group_name.to_s
-        puts entry
-      end
-    end
-
-    desc "stop", "Stop chrono. Must be started"
+    desc "stop #{GROUP_DESC}", "Stop chrono. Must be started."
     option :group, aliases: [:g]
     def stop
       conf.set_group(options[:group]) if options[:group]
@@ -46,45 +29,31 @@ module ChronoRb
       display_group
     end
 
-    desc "show", "Show chrono."
+    desc "del #{GROUP_DESC}", "Delete the last entry."
     option :group, aliases: [:g]
-    def show
+    def del
       conf.set_group(options[:group]) if options[:group]
-      puts "Showing chrono for group #{conf.group}"
+      del = Delete.new(config: conf)
+      del.del
+      puts "Deleting last entry #{del.del} for group #{conf.group}"
       display_group
     end
 
-    desc "test", "test chrono"
-    def test
-      conf.set_group('test-01')
-      start = Start.new(config: conf)
-      stop = Stop.new(config: conf)
-      start.start(DateTime.new(2017,3,13,9))
-      stop.stop(DateTime.new(2017,3,13,12,30))
-      start.start(DateTime.new(2017,3,13,13,30))
-      stop.stop(DateTime.new(2017,3,13,17,30))
-
-      start.start(DateTime.new(2017,3,14,9))
-      stop.stop(DateTime.new(2017,3,14,12,30))
-      start.start(DateTime.new(2017,3,14,13,30))
-      stop.stop(DateTime.new(2017,3,14,17,30))
-
-      start.start(DateTime.new(2017,3,15,9))
-      stop.stop(DateTime.new(2017,3,15,12,30))
-      start.start(DateTime.new(2017,3,15,13,30))
-      stop.stop(DateTime.new(2017,3,15,17,30))
-
-      start.start(DateTime.new(2017,3,16,9))
-      stop.stop(DateTime.new(2017,3,16,12,30))
-      start.start(DateTime.new(2017,3,16,13,30))
-      stop.stop(DateTime.new(2017,3,16,17,30))
-
-      start.start(DateTime.new(2017,3,17,9))
-      stop.stop(DateTime.new(2017,3,17,12,30))
-      start.start(DateTime.new(2017,3,17,13,30))
-      stop.stop(DateTime.new(2017,3,17,17,30))
-      puts "Starting chrono for group #{conf.group}"
+    desc "show #{GROUP_DESC}", "Show all chrono."
+    option :group, aliases: [:g]
+    def show
+      conf.set_group(options[:group]) if options[:group]
+      puts "Group #{conf.group}"
       display_group
+    end
+
+    desc "groups", "Display all existing groups."
+    def groups
+      conf.store.roots.map do |entry|
+        next if entry == conf.group_name.to_s
+        size = conf.store.fetch(entry, []).count
+        puts "name: #{entry} size: #{size}"
+      end
     end
 
     no_commands do
@@ -93,7 +62,6 @@ module ChronoRb
       end
 
       def display_group
-        puts "all:"
         Entries.new(config: conf).display
       end
     end
